03c4020a1072650dd0162df6dcce56e1f941f393
Justin Carruthers <jtcruthers@gmail.com>
2016-12-17 15:28:10 -0500

    blogs/new works

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,34 +1,35 @@
 var express = require('express');
 var Blog = require('../models/blog');
+var helpers = require('./helpers');
 var router = express.Router();
+var loggedIn = helpers.loggedIn;
 
-router.get('/', function (req, res) {
+router.get('/', loggedIn, function (req, res) {
     Blog.find({}, function(err, blogs) {
         console.log(blogs);
         res.render('blogs', {blogs: blogs, user: req.user});
     });
 });
 
-router.get('/:id', function(req, res) {
-    Blog.findOne({id: req.params.id}, function(err, blog) {
+router.get('/new', loggedIn, function(req, res) {
+    res.render('new', {user: req.user});
+});
+
+router.get('/:id', loggedIn, function(req, res) {
+    Blog.findById(req.params.id, function(err, blog) {
         res.render('showBlog', {blog: blog, user: req.user});
     });
 });
 
-router.get('/new', function(req, res) {
-    res.render('new', {user: req.user});
-});
-
-router.post('/new', function(req, res) {
-    Blog.register(new Blog({ title: req.body.title}), req.body.content, function(err, blog) {
+router.post('/new', loggedIn, function(req, res) {
+    var blog = new Blog({ title: req.body.title, content: req.body.content}); 
+    blog.save(function(err) {
         if (err) {
             return res.render('blogs', {user: req.user});
         }
-
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('showBlog', {blog: blog, user: req.user});
-        });
     });
+    
+    res.redirect(blog.id);
 });
 
 module.exports = router;

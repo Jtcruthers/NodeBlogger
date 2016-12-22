c6e7b8f895d2a7373fbd2b93916b367f7886ce0c
Justin Carruthers <jtcruthers@gmail.com>
2016-12-22 01:07:35 -0500

    added user to error page as well

--- app.js
+++ app.js
@@ -66,7 +66,8 @@ if (app.get('env') === 'development') {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: err,
+            user: req.user
         });
     });
 }
@@ -77,7 +78,8 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        error: {},
+        user: req.user
     });
 });
 

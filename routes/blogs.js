8eb3aac9d61fc9207bfe34272cb15d0de1afbaac
Justin Carruthers <jtcruthers@gmail.com>
2016-12-22 01:08:30 -0500

    refactored views

--- routes/blogs.js
+++ routes/blogs.js
@@ -7,17 +7,17 @@ var loggedIn = helpers.loggedIn;
 router.get('/', loggedIn, function (req, res) {
     Blog.find({}, function(err, blogs) {
         console.log(blogs);
-        res.render('blogs', {blogs: blogs, user: req.user});
+        res.render('blogs/blogs', {blogs: blogs, user: req.user});
     });
 });
 
 router.get('/new', loggedIn, function(req, res) {
-    res.render('new', {user: req.user});
+    res.render('blogs/new', {user: req.user});
 });
 
 router.get('/:id', loggedIn, function(req, res) {
     Blog.findById(req.params.id, function(err, blog) {
-        res.render('showBlog', {blog: blog, user: req.user});
+        res.render('blogs/showBlog', {blog: blog, user: req.user});
     });
 });
 
@@ -25,7 +25,7 @@ router.post('/new', loggedIn, function(req, res) {
     var blog = new Blog({ title: req.body.title, content: req.body.content}); 
     blog.save(function(err) {
         if (err) {
-            return res.render('blogs', {user: req.user});
+            return res.render('blogs/blogs', {user: req.user});
         }
     });
     
--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,13 @@ router.get('/', function (req, res) {
 });
 
 router.get('/register', function(req, res) {
-    res.render('register', { });
+    res.render('users/register', { });
 });
 
 router.post('/register', function(req, res) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
-            return res.render('register', { account : account });
+            return res.render('users/register', { account : account });
         }
 
         passport.authenticate('local')(req, res, function () {
@@ -26,7 +26,7 @@ router.post('/register', function(req, res) {
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+    res.render('users/login', { user : req.user });
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
@@ -39,14 +39,14 @@ router.get('/logout', function(req, res) {
 });
 
 router.get('/settings', helpers.loggedIn, function(req, res) {
-    res.render('userSettings', {user: req.user});
+    res.render('users/userSettings', {user: req.user});
 });
 
 router.get('/:username', helpers.loggedIn, function(req, res) {
     Account.findOne({username: req.params.username}, function(err, account) {
         console.log(req.params.username);
         console.log(account);
-        res.render('showUser', {user: account});
+        res.render('users/showUser', {user: account});
     });
 });
 
